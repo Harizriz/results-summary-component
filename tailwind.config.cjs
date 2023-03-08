06804a8f012db5ebd4d1ea60e1dfd0b741971df2
Hariz Mohd Hafiz Tay <hariztay.work@gmail.com>
2023-03-08 23:13:02 +0800

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    screens: {
+      sm: '375px',
+      // => @media (min-width: 375px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Hanken Grotesk', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'light-red': 'hsl(0, 100%, 67%)',
+        'orangey-yellow': 'hsl(39, 100%, 56%)',
+        'green-teal': 'hsl(166, 100%, 37%)',
+        'cobalt-blue': 'hsl(234, 85%, 45%)',
+        'light-slate-blue': 'hsl(252, 100%, 67%)',
+        'light-royal-blue': 'hsl(241, 81%, 54%)',
+        'violet-blue': 'hsla(256, 72%, 46%, 1)',
+        'persian-blue': 'hsl(241, 72%, 46%)',
+        'pale-blue': 'hsl(221, 100%, 96%)',
+        'light-lavender': 'hsl(241, 100%, 89%)',
+        'dark-gray-blue': 'hsl(224, 30%, 27%)',
+      },
+    },
+  },
+  plugins: [require('prettier-plugin-tailwindcss')],
+}
